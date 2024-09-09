c093e239f75a6fc42e3dc11c1bc331f3ca45f5d0
KayneTheBurr <kainejohnson63@gmail.com>
2024-09-09 10:16:32 -0700

    Initial
    Git setup

---
+++ Test Project/Assets/IntelliTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntelliTest : MonoBehaviour
+{
+    int kaineNums;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kaineNums = 1;
+        kaineNums++;
+    }
+
+   
+}
